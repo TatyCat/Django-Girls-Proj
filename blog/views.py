a21e91abcd93e11c686f167c6a955e8df46d4191
TatyCat <williams.tatyanna@gmail.com>
2018-01-12 18:58:19 -0500

    Added functionality for posting new blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -17,9 +19,24 @@ def post_detail(request,pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect ('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form':form})
 
 """
+commit=False means that we don't want to save the Post model yet 
+    – we want to add the author first. 
+Most of the time you will use form.save() without commit=False
+
 def details(request,id):
     post = Posts.objects.get(id=id)
     contexxt = {
